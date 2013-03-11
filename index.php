f1efd5c4ebb79ab8bb3553aded47620459f2066e
René Oelke <rene.oelke@gmail.com>
2013-03-11 12:21:16 +0100

    Added logging example.

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-phpinfo();
+$logPathName = __DIR__ . '/log';
+$logFileName = $logPathName . '/php-magazin.log';
+$logFileContent = '';
+
+is_dir($logPathName) || mkdir($logPathName);
+
+file_put_contents($logFileName, time() . PHP_EOL, FILE_APPEND);
+$logFileContent = file_get_contents($logFileName);
+
+echo '<h1>Logfile (' . $logFileName . ')</h1>';
+echo '<pre>' . $logFileContent . '</pre>';
